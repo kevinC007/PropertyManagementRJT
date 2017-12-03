38b86a6771dd9deeb26110fb8bb141f5c7d67566
Joel <joelaranha17@gmail.com>
2017-12-03 14:09:22 -0600

    DashBoard Xml Updated. Fragments for dashboard created and linked AddPropertyPage added

---
+++ app/src/main/java/com/example/shuangzhecheng/propertymanagementrjt/FragmentAddProperty.java
@@ -0,0 +1,26 @@
+package com.example.shuangzhecheng.propertymanagementrjt;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by joel on 12/3/2017.
+ */
+
+public class FragmentAddProperty extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_addproperty, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
